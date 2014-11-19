39cc535cd05b6ed9f203b99f7ef8dbbf6d4aaefc
misterrios <misterrios@gmail.com>
2014-11-19 22:41:33 +0100

    finished ch01

--- ch01/project_name/project_name.py
+++ ch01/project_name/project_name.py
@@ -9,31 +9,34 @@ from django.http import HttpResponse
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 
-SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+SECRET_KEY = os.environ.get('SECRET_KEY', {{ secret_key }})
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
-def index(request):
-    return HttpResponse('Hello World')
-
-urlpatterns = (
-    url(r'^$', index),
-    )
 
 settings.configure(
     DEBUG=DEBUG,
-    SECRET_KEY=SECRET_KEY,
-    ALLOWED_HOSTS=ALLOWED_HOSTS,    
+    SECRET_KEY=SECRET_KEY,g
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
     ),
 )
 
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+urlpatterns = (
+    url(r'^$', index),
+    )
+
+
 application = get_wsgi_application()
+
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
