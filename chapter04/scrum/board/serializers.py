4184131efc223e63cc725d1fbeee66eb23b706df
misterrios <misterrios@gmail.com>
2015-03-30 22:31:20 +0200

    chapter04: added serializers

---
+++ chapter04/scrum/board/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+
+from .models import Sprint, Task
+
+class SprintSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Sprint
+        fields = ('id', 'name', 'description', 'end', )
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    status_display = serializers.SerializerMethodField('get_status_display')
+
+    class Meta:
+        model = Task
+        fields = ('id', 'name', 'description', 'sprint', 
+            'status', 'status_display', 'order',
+            'assigned', 'started', 'due', 'completed', )
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
--- chapter04/scrum/board/views.py
+++ chapter04/scrum/board/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Sprint
+from .serializers import SprintSerializer
+
+
+class DefaultMixin(object):
+    """
+    Default settings for view authentication, permissions,
+    filtering, and pagination
+    """
+
+    authentication_classes = (
+        authentication.BasicAuthentication,
+        authentication.TokenAuthentication,
+        )
+    permission_classes = (
+        permissions.IsAuthenticated,
+        )
+    paginate_by = 25
+    paginate_by_param = "page_size"
+    max_paginate_by = 100
+    
+
+
+class SprintViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint for listing and creating Sprints.
+    """
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
+
